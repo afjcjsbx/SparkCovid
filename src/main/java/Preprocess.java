13a58e2ce61d08d1a06813da7140e6190c9e5d1b
Mauro <afjcjsbx@gmail.com>
2020-05-08 17:16:28 +0200

    1.0.0

--- src/main/java/Preprocess.java
+++ src/main/java/Preprocess.java
@@ -1,56 +1,64 @@
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.util.Progressable;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
+import scala.Tuple2;
 import utils.CSVReaderInJava;
 import utils.Covid1Data;
 import utils.FileUtils;
 
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Preprocess {
 
-    public static void main(String[] args) {
-
-        String pathFileWeatherDescription = "hdfs://master:9870/dpc-covid19-ita-andamento-nazionale.csv";
-
-        //-------------------------------------------------------
-
-        //local mode
-        SparkConf conf = new SparkConf()
-                .setMaster("local")
-                .setAppName("Query 1");
+    String pathFileWeatherDescription = "hdfs://127.0.0.1:54310/dpc-covid19-ita-andamento-nazionale.csv";
 
 
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        sc.setLogLevel("ERROR");
+    public static void main(String[] args) throws IOException, URISyntaxException {
 
 
-        long start = System.currentTimeMillis();
-
-
-        //read file
-        JavaRDD<String> file= sc.textFile(pathFileWeatherDescription);
-        String header = file.first();
-        String[] firstLine = header.split(",",-1);
-
-
-        //get ther other lines of csv file
-        long iParseFile = System.currentTimeMillis();
-        JavaRDD<String> otherLines = file.filter(row -> !row.equals(header));
-        //JavaRDD<ArrayList<Covid1Data>>  listOflistOfcities = otherLines.map(line -> CSVReaderInJava.createData(line));
-        long fParseFile = System.currentTimeMillis();
-
-
-
-        long stop = System.currentTimeMillis();
-        System.out.println("ELAPSED TIME: ----> " + (stop-start)/1000);
-
-        sc.stop();
+        //1. Get the instance of COnfiguration
+        Configuration configuration = new Configuration();
+        //2. Create an InputStream to read the data from local file
+        InputStream inputStream = new BufferedInputStream(new FileInputStream("src/main/resources/shakespeare.txt"));
+        //3. Get the HDFS instance
+        FileSystem hdfs = FileSystem.get(new URI("hdfs://127.0.0.1:54310"), configuration);
+        //4. Open a OutputStream to write the data, this can be obtained from the FileSytem
+        OutputStream outputStream = hdfs.create(new Path("hdfs://127.0.0.1:54310/Hadoop_File.txt"),
+                new Progressable() {
+                    @Override
+                    public void progress() {
+                        System.out.println("....");
+                    }
+                });
+        try
+        {
+            IOUtils.copyBytes(inputStream, outputStream, 4096, false);
+        }
+        finally
+        {
+            IOUtils.closeStream(inputStream);
+            IOUtils.closeStream(outputStream);
+        }
     }
+
 }
