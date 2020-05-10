f2ab4f8c02ac70c50c43449b0667c5f97f066bd8
Mauro <afjcjsbx@gmail.com>
2020-05-10 11:50:50 +0200

    1.0.1

--- src/main/java/Preprocess.java
+++ src/main/java/Preprocess.java
@@ -1,56 +1,78 @@
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.util.Progressable;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
+import scala.Tuple2;
 import utils.CSVReaderInJava;
 import utils.Covid1Data;
 import utils.FileUtils;
 
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Preprocess {
 
-    public static void main(String[] args) {
+    String pathFileWeatherDescription = "hdfs://127.0.0.1:54310/dpc-covid19-ita-andamento-nazionale.csv";
 
-        String pathFileWeatherDescription = "hdfs://0.0.0.0.:54310/dpc-covid19-ita-andamento-nazionale.csv";
 
-        //-------------------------------------------------------
+    public static void main(String[] args) throws IOException, URISyntaxException {
 
-        //local mode
+        /*
+                if (args.length != 3) {
+            System.out.println("\nERROR: Insert arguments in this order: " +
+                    "1. 'file city-attributes, 2. file temperatures 3. redis ip'");
+        }
+        */
+
+        long initialTime = System.currentTimeMillis();
+
         SparkConf conf = new SparkConf()
                 .setMaster("local")
-                .setAppName("Query 1");
-
-
+                .setAppName("Hello World");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        sc.setLogLevel("ERROR");
 
+        long iOperations = System.currentTimeMillis();
 
-        long start = System.currentTimeMillis();
-
-
-        //read file
-        JavaRDD<String> file= sc.textFile(pathFileWeatherDescription);
-        String header = file.first();
+        JavaRDD<String> input = sc.textFile("src/main/resources/dataset1.csv");
+        String header = input.first();
         String[] firstLine = header.split(",",-1);
 
 
+    /*
         //get ther other lines of csv file
         long iParseFile = System.currentTimeMillis();
-        JavaRDD<String> otherLines = file.filter(row -> !row.equals(header));
-        //JavaRDD<ArrayList<Covid1Data>>  listOflistOfcities = otherLines.map(line -> CSVReaderInJava.createData(line));
+        JavaRDD<String> otherLines = input.filter(row -> !row.equals(header));
+        JavaRDD<ArrayList<Covid1Data>>  weeklyDate = otherLines
+                .map(line -> DataParser.parseCSV(line));
         long fParseFile = System.currentTimeMillis();
 
+        weeklyDate.collect();
 
 
-        long stop = System.currentTimeMillis();
-        System.out.println("ELAPSED TIME: ----> " + (stop-start)/1000);
+     */
 
+        long fOperations = System.currentTimeMillis();
         sc.stop();
+        long finalTime = System.currentTimeMillis();
+        System.out.printf("Total time to complete: %s ms\n", Long.toString(finalTime-initialTime));
     }
+
 }
--- src/main/java/utils/Covid1Data.java
+++ src/main/java/utils/Covid1Data.java
@@ -17,22 +17,6 @@ public class Covid1Data {
     private int tamponi;
     private int casi_testati;
 
-    public Covid1Data(String data, String stato, int ricoverati_con_sintomi, int terapia_intensiva, int totale_ospedalizzati, int isolamento_domiciliare, int totale_positivi, int variazione_totale_positivi, int nuovi_positivi, int dimessi_guariti, int deceduti, int totale_casi, int tamponi, int casi_testati) {
-        this.data = data;
-        this.stato = stato;
-        this.ricoverati_con_sintomi = ricoverati_con_sintomi;
-        this.terapia_intensiva = terapia_intensiva;
-        this.totale_ospedalizzati = totale_ospedalizzati;
-        this.isolamento_domiciliare = isolamento_domiciliare;
-        this.totale_positivi = totale_positivi;
-        this.variazione_totale_positivi = variazione_totale_positivi;
-        this.nuovi_positivi = nuovi_positivi;
-        this.dimessi_guariti = dimessi_guariti;
-        this.deceduti = deceduti;
-        this.totale_casi = totale_casi;
-        this.tamponi = tamponi;
-        this.casi_testati = casi_testati;
-    }
 
     public String getData() {
         return data;
