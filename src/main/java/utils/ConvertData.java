89c984b50d254fe25cfd49ce1e8f0fd25fc3dff3
stirperichard <richard.stirpe@alumni.uniroma2.eu>
2020-05-12 17:57:21 +0200

    Modificato ConvertData

--- src/main/java/utils/ConvertData.java
+++ src/main/java/utils/ConvertData.java
@@ -2,12 +2,9 @@ package utils;
 
 import java.text.ParseException;
 import java.util.Calendar;
-import java.util.Date;
-import java.text.SimpleDateFormat;
 
 public class ConvertData {
-    public static Date convert(String data){
-        Date date = null;
+    public static Calendar convert(String data){
         String[] dataSplitted = data.split("/", -1);
         int day = Integer.parseInt(dataSplitted[1]);
         int month = Integer.parseInt(dataSplitted[0]);
@@ -15,28 +12,22 @@ public class ConvertData {
 
         year += 2000;
 
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MMM/yyyy");
-        String dateInString = day + "/" + month + "/" + year;
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month, day);
+        return cal;
+    }
 
-        try {
+    public static Calendar addMonth(Calendar date) {
 
-            date = formatter.parse(dateInString);
-            System.out.println(date);
-            System.out.println(formatter.format(date));
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        date.add(Calendar.MONTH, 1);
 
         return date;
     }
 
-    public static Calendar addMonth(Date date) throws ParseException {
+    public static Calendar nextMonth(Calendar cal) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Date new_date = sdf.parse(String.valueOf(date));
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new_date);
+        cal.set(Calendar.DAY_OF_MONTH, 31);
+        cal.add(Calendar.MONTH, 1);
 
         return cal;
     }
